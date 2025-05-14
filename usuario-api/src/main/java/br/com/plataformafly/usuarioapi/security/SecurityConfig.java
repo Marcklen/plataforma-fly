9764e67b33d9eecbb976853821d97368d80939ee
marcklen <marcklen@icloud.com>
2025-05-14 16:49:55 -0300

    feat: add CORS para acessar o front

--- usuario-api/src/main/java/br/com/plataformafly/usuarioapi/security/SecurityConfig.java
+++ usuario-api/src/main/java/br/com/plataformafly/usuarioapi/security/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
     private static final String[] AUTH_WHITELIST = {
             "/h2-console/**",
             "/swagger-ui/**",
-            "/v3/api-docs/**"
+            "/v3/api-docs/**",
+            "/actuator/**",
     };
 
     @Bean
