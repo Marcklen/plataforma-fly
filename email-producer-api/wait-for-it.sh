4c3d36f355f4440663fd950b42d8b5e1927711f2
marcklen <marcklen@icloud.com>
2025-05-27 16:47:17 -0300

    fix: ajustado arquivos Dockerfile, wait-for-it.sh e docker-compose.yml

--- email-producer-api/wait-for-it.sh
+++ email-producer-api/wait-for-it.sh
@@ -3,12 +3,11 @@
 HOST=$1
 PORT=$2
 shift 2
-CMD="$@"
 
 until nc -z "$HOST" "$PORT"; do
   echo "Aguardando $HOST:$PORT..."
   sleep 2
 done
 
-echo "$HOST:$PORT está disponível, executando comando: $CMD"
+echo "$HOST:$PORT está disponível, executando comando: $@"
 exec "$@"
